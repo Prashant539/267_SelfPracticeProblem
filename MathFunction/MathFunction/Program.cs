fecb0270d7935bd1d083a860fe8507468461a07b
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-12 16:01:14 +0530

    [Prashant]Add: Swaping of two numbers

---
+++ MathFunction/MathFunction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MathFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Program");
+            SwapNumber swapNumber = new SwapNumber();
+            swapNumber.SwapNumberPrint();
+        }
+    }
+}
+    
---
+++ MathFunction/MathFunction/SwapNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFunction
+{
+    public class SwapNumber
+    {
+        int a, b, temp;
+
+        public void SwapNumberPrint()
+        {
+            Console.WriteLine("Enter the First Number to Swap: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Number to Swap: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before the Swap a = {0}, b = {1}", a, b);
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After the Swap a = {0}, b = {1}", a, b);
+        }
+    }
+}
